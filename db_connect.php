b32353080fcf8221282f8269bba0b9a63c867486
ethanisawesome309-netizen <ethanisawesome309@gmail.com>
2025-12-18 17:36:36 -0500

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -5,16 +5,20 @@ $user = "rnobncovhs@intelliresolvers-server";
 $pass = "Leafsarethebest1!";
 $charset = "utf8mb4";
 
+$ssl_ca = __DIR__ . '/BaltimoreCyberTrustRoot.crt.pem'; 
+
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::MYSQL_ATTR_SSL_CA       => $ssl_ca, 
 ];
 
 try {
     $conn = new PDO($dsn, $user, $pass, $options);
+    echo "Connected successfully!";
 } catch (PDOException $e) {
-    die("Database connection failed");
+    die("Database connection failed: " . $e->getMessage());
 }
