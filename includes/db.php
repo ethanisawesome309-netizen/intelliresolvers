599d0aca73363a879ada473edc493c0de9c7e306
ethanisawesome309-netizen <ethanisawesome309@gmail.com>
2026-01-02 13:50:32 -0500

    Add files via upload

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = "intelliresolvers.mysql.database.azure.com";
 $db   = "appdb";
-$user = "ethanwang";
-$pass = "Wzxwzx07";
+$user = "ethanwang@intelliresolvers";
+$pass = getenv("DB_PASSWORD");
 $port = 3306;
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
@@ -10,11 +10,12 @@ $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
 ];
 
 try {
     $conn = new PDO($dsn, $user, $pass, $options);
-    echo "CONNECTED (NON-SSL)";
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die("Database connection failed");
 }
