268911254d99a3030a0ef6586c81fb423068433f
ethanisawesome309-netizen <ethanisawesome309@gmail.com>
2026-01-02 13:52:38 -0500

    Add files via upload

--- includes/db.php
+++ includes/db.php
@@ -10,12 +10,11 @@ $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-
     PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
 ];
 
 try {
     $conn = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
-    die("Database connection failed");
+    die("Database connection failed") . $e->getMessage();
 }
