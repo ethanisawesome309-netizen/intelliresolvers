a98228cd9944b3da25a11ca4ef6e4f51f84ff08b
ethanisawesome309-netizen <ethanisawesome309@gmail.com>
2026-01-02 14:19:48 -0500

    Add files via upload

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = "intelliresolvers.mysql.database.azure.com";
 $db   = "appdb";
-$user = "ethanwang";
-$pass = "Wzxwzx07";
+$user = "ethanwang";   
+$pass = "Wzxwx07";
 $port = 3306;
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
@@ -11,7 +11,7 @@ $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 
-    // 🔐 SSL enabled, verification skipped (Windows App Service)
+    // 🔐 SSL REQUIRED BY SERVER, WINDOWS TRUST STORE USED
     PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
 ];
 
