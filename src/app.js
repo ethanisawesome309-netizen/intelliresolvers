adedeee7b1efa0f01c7c0d6a8f86dcb0320aae2b
ethanisawesome309-netizen <ethanisawesome309@gmail.com>
2026-01-02 02:45:27 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth.routes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+module.exports = app;
