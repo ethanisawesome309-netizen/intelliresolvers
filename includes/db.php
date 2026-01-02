7e28487599ed367c2217983d9cab8580c0bd0523
ethanisawesome309-netizen <ethanisawesome309@gmail.com>
2026-01-02 12:55:11 -0500

    Add files via upload

--- includes/db.php
+++ includes/db.php
@@ -2,20 +2,16 @@
 $host = "intelliresolvers.mysql.database.azure.com";
 $db   = "appdb";
 $user = "ethanwang";
-$pass = "Wzxwzx07"; // use env var
+$pass = "Wzxwzx07";
 $port = 3306;
 
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4;sslmode=verify_ca";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::MYSQL_ATTR_SSL_CA => __DIR__ . "/DigiCertGlobalRootCA.crt.pem",
-];
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+
+    PDO::MYSQL_ATTR_SSL_CA => __DIR__ . "/DigiCertGlobalRootCA.crt.pem",
 ];
 
 try {
