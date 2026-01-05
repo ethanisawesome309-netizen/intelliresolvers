f5af4b7d95a01f385403cd5272062ffd9c9ebe57
ethanisawesome309-netizen <ethanisawesome309@gmail.com>
2026-01-05 16:43:44 -0500

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,3 @@
-<?php
-session_start();
-
-// Redirect if not logged in
-if (!isset($_SESSION["user_id"])) {
-    header("Location: signinpage.php");
-    exit;
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,15 +5,13 @@ if (!isset($_SESSION["user_id"])) {
   <title>Dashboard</title>
 </head>
 <body>
-  <!-- React will render inside this div -->
   <div id="root"></div>
 
-  <!-- Optional: red warning if React fails -->
   <noscript style="color:red;">
     JavaScript is required to load the dashboard.
   </noscript>
 
-  <!-- Load React JS -->
-  <script src="/assets/index.js"></script>
+  <!-- Correct: Vite + React bundle -->
+  <script type="module" src="/assets/index.js"></script>
 </body>
 </html>
