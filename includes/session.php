1f384d8f8643cdb30625e43a71cd61a5ce376fac
ethanisawesome309-netizen <ethanisawesome309@gmail.com>
2026-01-08 13:43:15 -0500

    Add files via upload

--- includes/session.php
+++ includes/session.php
@@ -3,9 +3,6 @@
 
 if (session_status() === PHP_SESSION_NONE) {
 
-    ini_set('session.save_handler', 'files');
-    ini_set('session.save_path', __DIR__ . '/../sessions');
-
     session_set_cookie_params([
         'path' => '/',
         'secure' => true,
