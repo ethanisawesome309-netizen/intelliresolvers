8b319a5163e26488dd2fb0053d73361971372a03
ethanisawesome309-netizen <ethanisawesome309@gmail.com>
2026-01-08 13:31:19 -0500

    Add files via upload

--- includes/session.php
+++ includes/session.php
@@ -1,14 +1,17 @@
 <?php
-ini_set('session.use_strict_mode', 1);
+// /includes/session.php
 
-session_set_cookie_params([
-    'lifetime' => 0,
-    'path' => '/',
-    'domain' => 'intelliresolvers.com',
-    'secure' => true,
-    'httponly' => true,
-    'samesite' => 'Lax',
-]);
+if (session_status() === PHP_SESSION_NONE) {
 
-session_name('INTELLISESSID');
-session_start();
+    ini_set('session.save_handler', 'files');
+    ini_set('session.save_path', __DIR__ . '/../sessions');
+
+    session_set_cookie_params([
+        'path' => '/',
+        'secure' => true,
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]);
+
+    session_start();
+}
